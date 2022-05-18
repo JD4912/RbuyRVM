e6a140d9fe19670ad9fda1059ed84aea854f2d12
JD4912 <58825633+JD4912@users.noreply.github.com>
2022-05-18 13:41:11 +0530

    Create script.sh

---
+++ scripts/script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -L get.rvm.io | bash -s stable
+source /etc/profile.d/rvm.sh
+rvm requirements
+rvm install 2.7.2
+gem install rails -v 6.5.1
+cd /Newruby
+bundle install
+rails server -b 0.0.0.0
